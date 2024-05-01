5b4dbef8975a0035fa5fe04fb51477d992b82833
kadir-zeren <k4dzeren@gmail.com>
2024-05-01 10:49:29 +0300

    python intro

--- python.py
+++ python.py
@@ -1,7 +1,24 @@
 print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
+print("I'm great")
+print('''Dont't do it" said the manager''')
+print('''bu birinci satir
+      bu ikinci satir
+      bu uc 
+      bu dort''')
 
+print('3.14')
+print(3.14)
+print('3.14'*3)
+print(3.14*3)
+print(567)
 
+print('bir')
+print()
+print('uc')
+print('I\'m great')
+print("\"don't do it\" said the manager")
+print('bu benim cumlem \nbu ikinci satir')
+print('bu benim cumlem \tbu ikinci satir')
+print('sdfsdfsdfsdf sdf sdfsd fsdf sdfsdf sdf sd/ dsf \
+      dasdasdasd asd asd asd asd asd asd asd asd  \
+      ')
